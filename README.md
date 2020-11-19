485641f7d3b4ce91e2cda6ec79eb08082fcdfb57
Henrique Poleselo <hpoleselo@gmail.com>
2020-11-19 03:33:41 -0800

    Added JS101

---
+++ README.md
@@ -0,0 +1,12 @@
+# My Own Introduction to JavaScript
+This is my own documentation for JS.
+
+## Installation
+I'm using VSCode, so i recommend to myself using the snippets for autocompletion and Live Server, Boilerplate.
+NodeJS 
+
+## vanilla-js
+Basics of JS. You can find a README with my notes on JS as well and my *overcommented* code in JS.
+
+## ownweb
+When i get to know JS correctly i will build my own website, without freaking bootstrap.
---
+++ vanilla-js/vanilla.js
@@ -0,0 +1,49 @@
+/* Before jumping into NodeJS, learning the basics of JS, which is the syntax and the browser-oriented programming
+i.e: to give our website a behaviour, I think it's good to cover the basics with vanilla itself so we can then
+later differ one from another and know what is pure JS and what is not. */
+
+// Declaring variables, in this case, global scope.
+var x,y;
+var string;
+
+function sum (x,y) {
+    return x + y;
+}
+
+function returnString (strng){
+    return strng
+}
+
+/* From what I've seen it's not normal to have a main function (0not a practice) in JS, but I'm using it anyway in this case just for test.
+Open Chrome/Mozilla up and check on the console tab under Development Tools (Inspector) and you will see the outputs from this function */
+function main() {
+    x = 5;
+    y = 5;
+    strng = "Surfing is magic";
+    console.log("Sum is: " + sum(x,y));
+    console.log("String is: " + returnString(strng));
+}
+
+main()
+
+
+function testingOperators() {
+    /* In JS there are 3 operators: =, == and ===
+   Where = means attributing a value
+   Where == means validation
+   Where ==== means value and ...
+
+   Check the example below
+    */
+   strng
+
+}
+
+function aboutECS() {
+
+}
+
+
+function aboutArrowFunctions() {
+
+}
